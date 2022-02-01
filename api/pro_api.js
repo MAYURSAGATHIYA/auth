1b46c3e81959fadd251602b9bb01747537fa5041
Mayur <mayursagathiya98@gmail.com>
2022-02-01 13:19:03 +0530

    upgarded

---
+++ api/pro_api.js
@@ -0,0 +1,37 @@
+const  { getAll, getById, removeById, save, update } = require('..dal/prod');
+
+const createpro=async({first,last,email,password,confirmpassword})=>{
+
+
+    const pro={
+        first,
+        last,
+        email,
+        password,
+        confirmpassword
+    }
+    return await save(pro);
+}
+
+const getpros=async()=>{
+    return await getAll();
+}
+
+const getpro=async id =>{
+    return await getById(id);
+}
+
+const delpro=async id=>{
+    return await removeById(id);
+}
+const uppro=async (id,{first,last,email,password,confirmpassword})=>{
+    return await update(id,{first,last,email,password,confirmpassword})
+}
+
+module.exports={
+    createpro,
+    getpros,
+    getpro,
+    delpro,
+    uppro
+}
