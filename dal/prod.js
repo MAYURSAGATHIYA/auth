c6bc6d89973af8218d8b8b2ad8554897e186a4a6
Mayur <mayursagathiya98@gmail.com>
2022-02-01 12:56:50 +0530

    databse

--- dal/prod.js
+++ dal/prod.js
@@ -1,17 +1,33 @@
-const products= require('./index').db('CRED').collection('user');
+const products = require('./index').db('CRED').collection('user');
 
-const ObjectId=require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
-
-const save=async({first,last,email,password,confirmpassword})=>{
-    const result = await products.insertOne({first,last,email,password,confirmpassword});
+// ================================================================
+const save = async ({ first, last, email, password, confirmpassword }) => {
+    const result = await products.insertOne({ first, last, email, password, confirmpassword });
 
     return result.ops[0];
 }
-const getAll=async()=>{
+// ================================================================
+const getAll = async () => {
 
-    const cursor=await products.find();
+    const cursor = await products.find();
 
     return cursor.toArray();
 }
 console.log("jhv")
+// ================================================================
+const getById = async (id) => {
+    return await products.findOne({ _id: ObjectId });
+}
+// ================================================================
+const update = async (id, { first, last, email, password, confirmpassword }) => {
+    const result = await products.replaceOne({ _id: ObjectId(id) }, { first, last, email, password, confirmpassword });
+    return result.ops[0];
+}
+// ================================================================
+const removeById = async id => {
+    await products.deleteOne({ _id: ObjectId(id) });
+}
+
+module.exports = { getAll, getById, removeById, save, update };
